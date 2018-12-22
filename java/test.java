b18ee7c9d9ce7119a91f9fd47cc0c449786bd034
SunTalk <infernal56727@gmail.com>
2018-12-22 17:36:42 +0800

    add poster and some java

---
+++ java/test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class test{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int tmp = in.nextInt();
+		System.out.print("Hello World!"+tmp);
+	}
+}
