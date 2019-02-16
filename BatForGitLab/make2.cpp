eca4f722aaf944b342e64ac0428d8e6f253f5ad6
SunTalk <infernal56727@gmail.com>
2019-02-16 17:42:11 +0800

    update

---
+++ BatForGitLab/make2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	freopen("Nothing2.txt","a",stdout);
+	srand( (unsigned)time(NULL) );
+
+	string str;
+	str = "";
+	int low,up;
+	low = 32;
+	up = 126;
+	int tmp;
+
+	for(int i = 0 ; i < 10 ; i++ ){
+		tmp = rand() % (up - low + 1) + low;
+		char add = tmp;
+		str = str + add;
+	}
+
+	cout << str ;
+
+	return 0;
+}
